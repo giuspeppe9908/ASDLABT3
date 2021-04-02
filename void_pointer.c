97d56a58d3e61f0217523556e1e7e2857dd9e91b
giuspeppe9908 <66006287+giuspeppe9908@users.noreply.github.com>
2021-04-02 21:30:18 +0200

    First Exercise
    It has to complete

---
+++ void_pointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "mylib.h"
+
+int main(){
+	ReadFile(fp);
+	sleep(1);
+	PrintStruct(total_records);
+	printf("Now applying Merge-BinaryInsertionSort...\n");
+	return 0;
+}
